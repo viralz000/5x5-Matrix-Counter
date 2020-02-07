ceee6a2c6af6df78f3fde8529172c66f55bae6d5
viralz000 <58083928+viralz000@users.noreply.github.com>
2020-02-07 23:13:23 +0800

    Add files via upload

---
+++ 5x5matrix counter/5x5matrix counter/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button20 = New System.Windows.Forms.Button()
+        Me.Button19 = New System.Windows.Forms.Button()
+        Me.Button18 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button25 = New System.Windows.Forms.Button()
+        Me.Button24 = New System.Windows.Forms.Button()
+        Me.Button23 = New System.Windows.Forms.Button()
+        Me.Button22 = New System.Windows.Forms.Button()
+        Me.Button21 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(132, 30)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 61)
+        Me.Button2.TabIndex = 8
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(233, 30)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 61)
+        Me.Button3.TabIndex = 9
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(336, 30)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(84, 61)
+        Me.Button4.TabIndex = 10
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(438, 30)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(84, 61)
+        Me.Button5.TabIndex = 11
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(438, 116)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(84, 61)
+        Me.Button10.TabIndex = 16
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(336, 116)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(84, 61)
+        Me.Button9.TabIndex = 15
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(233, 116)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(84, 61)
+        Me.Button8.TabIndex = 14
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(132, 116)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(84, 61)
+        Me.Button7.TabIndex = 13
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(33, 116)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(84, 61)
+        Me.Button6.TabIndex = 12
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(438, 203)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(84, 61)
+        Me.Button15.TabIndex = 21
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(336, 203)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(84, 61)
+        Me.Button14.TabIndex = 20
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(233, 203)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(84, 61)
+        Me.Button13.TabIndex = 19
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(132, 203)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(84, 61)
+        Me.Button12.TabIndex = 18
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(33, 203)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(84, 61)
+        Me.Button11.TabIndex = 17
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button20
+        '
+        Me.Button20.Location = New System.Drawing.Point(438, 287)
+        Me.Button20.Name = "Button20"
+        Me.Button20.Size = New System.Drawing.Size(84, 61)
+        Me.Button20.TabIndex = 26
+        Me.Button20.UseVisualStyleBackColor = True
+        '
+        'Button19
+        '
+        Me.Button19.Location = New System.Drawing.Point(336, 287)
+        Me.Button19.Name = "Button19"
+        Me.Button19.Size = New System.Drawing.Size(84, 61)
+        Me.Button19.TabIndex = 25
+        Me.Button19.UseVisualStyleBackColor = True
+        '
+        'Button18
+        '
+        Me.Button18.Location = New System.Drawing.Point(233, 287)
+        Me.Button18.Name = "Button18"
+        Me.Button18.Size = New System.Drawing.Size(84, 61)
+        Me.Button18.TabIndex = 24
+        Me.Button18.UseVisualStyleBackColor = True
+        '
+        'Button17
+        '
+        Me.Button17.Location = New System.Drawing.Point(132, 287)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(84, 61)
+        Me.Button17.TabIndex = 23
+        Me.Button17.UseVisualStyleBackColor = True
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.SystemColors.Control
+        Me.Button16.Location = New System.Drawing.Point(33, 287)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(84, 61)
+        Me.Button16.TabIndex = 22
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button25
+        '
+        Me.Button25.Location = New System.Drawing.Point(438, 368)
+        Me.Button25.Name = "Button25"
+        Me.Button25.Size = New System.Drawing.Size(84, 61)
+        Me.Button25.TabIndex = 31
+        Me.Button25.UseVisualStyleBackColor = True
+        '
+        'Button24
+        '
+        Me.Button24.Location = New System.Drawing.Point(336, 368)
+        Me.Button24.Name = "Button24"
+        Me.Button24.Size = New System.Drawing.Size(84, 61)
+        Me.Button24.TabIndex = 30
+        Me.Button24.UseVisualStyleBackColor = True
+        '
+        'Button23
+        '
+        Me.Button23.Location = New System.Drawing.Point(233, 368)
+        Me.Button23.Name = "Button23"
+        Me.Button23.Size = New System.Drawing.Size(84, 61)
+        Me.Button23.TabIndex = 29
+        Me.Button23.UseVisualStyleBackColor = True
+        '
+        'Button22
+        '
+        Me.Button22.Location = New System.Drawing.Point(132, 368)
+        Me.Button22.Name = "Button22"
+        Me.Button22.Size = New System.Drawing.Size(84, 61)
+        Me.Button22.TabIndex = 28
+        Me.Button22.UseVisualStyleBackColor = True
+        '
+        'Button21
+        '
+        Me.Button21.Location = New System.Drawing.Point(33, 368)
+        Me.Button21.Name = "Button21"
+        Me.Button21.Size = New System.Drawing.Size(84, 61)
+        Me.Button21.TabIndex = 27
+        Me.Button21.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(33, 30)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 61)
+        Me.Button1.TabIndex = 7
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(548, 462)
+        Me.Controls.Add(Me.Button25)
+        Me.Controls.Add(Me.Button24)
+        Me.Controls.Add(Me.Button23)
+        Me.Controls.Add(Me.Button22)
+        Me.Controls.Add(Me.Button21)
+        Me.Controls.Add(Me.Button20)
+        Me.Controls.Add(Me.Button19)
+        Me.Controls.Add(Me.Button18)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button20 As Button
+    Friend WithEvents Button19 As Button
+    Friend WithEvents Button18 As Button
+    Friend WithEvents Button17 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button25 As Button
+    Friend WithEvents Button24 As Button
+    Friend WithEvents Button23 As Button
+    Friend WithEvents Button22 As Button
+    Friend WithEvents Button21 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Button1 As Button
+End Class
---
+++ 5x5matrix counter/5x5matrix counter/Form1.vb
@@ -0,0 +1,150 @@
+﻿Public Class Form1
+
+    Dim inputs(,) As Boolean = {
+        {False, False, False, False},
+        {False, False, False, True},
+        {False, False, True, False},
+        {False, False, True, True},
+        {False, True, False, False},
+        {False, True, False, True},
+        {False, True, True, False},
+        {False, True, True, True},
+        {True, False, False, False},
+        {True, False, False, True}
+    }
+    Dim second As Int32 = -1
+
+    Dim A As Boolean
+    Dim B As Boolean
+    Dim C As Boolean
+    Dim D As Boolean
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        second += 1
+
+        A = inputs(second, 0)
+        B = inputs(second, 1)
+        C = inputs(second, 2)
+        D = inputs(second, 3)
+
+        Button1.BackColor = Color.Red
+        Button7.BackColor = Color.White
+        Button17.BackColor = Color.White
+
+        If (Not B) Or D Or C Then
+            Button2.BackColor = Color.Red
+            Button3.BackColor = Color.Red
+            Button23.BackColor = Color.Red
+        Else
+            Button2.BackColor = Color.White
+            Button3.BackColor = Color.White
+            Button23.BackColor = Color.White
+        End If
+
+        If (Not D) Or C Or B Or A Then
+            Button4.BackColor = Color.Red
+        Else
+            Button4.BackColor = Color.White
+        End If
+
+        If C Or A Or ((Not B) And (Not D)) Or (B And D) Then
+            Button5.BackColor = Color.Red
+        Else
+            Button5.BackColor = Color.White
+        End If
+
+        If A Or ((Not C) And (Not D)) Or (B And (Not C)) Or (B And (Not D)) Then
+            Button6.BackColor = Color.Red
+        Else
+            Button6.BackColor = Color.White
+        End If
+
+        If (Not A) And (Not B) And (Not C) And D Then
+            Button8.BackColor = Color.Red
+        Else
+            Button8.BackColor = Color.White
+        End If
+
+        If (B And (Not C) And (Not D)) Or (B And C And D) Then
+            Button9.BackColor = Color.Red
+        Else
+            Button9.BackColor = Color.White
+        End If
+
+        If A Or ((Not B) And (Not D)) Or ((Not B) And C) Then
+            Button10.BackColor = Color.Red
+        Else
+            Button10.BackColor = Color.White
+        End If
+
+        If (Not D) Or A Or ((Not B) And C) Or (B And (Not C)) Then
+            Button11.BackColor = Color.Red
+            Button15.BackColor = Color.Red
+        Else
+            Button11.BackColor = Color.White
+            Button15.BackColor = Color.White
+        End If
+
+        If A Or ((Not B) And C) Or (C And (Not D)) Or (B And (Not C)) Then
+            Button12.BackColor = Color.Red
+            Button14.BackColor = Color.Red
+        Else
+            Button12.BackColor = Color.White
+            Button14.BackColor = Color.White
+        End If
+
+        If D Or C Or B Or A Then
+            Button13.BackColor = Color.Red
+        Else
+            Button13.BackColor = Color.White
+        End If
+
+        If ((Not B) And (Not D)) Or (C And (Not D)) Then
+            Button16.BackColor = Color.Red
+        Else
+            Button16.BackColor = Color.White
+        End If
+
+        If (B And C And D) Or ((Not A) And (Not B) And (Not C) And D) Then
+            Button18.BackColor = Color.Red
+        Else
+            Button18.BackColor = Color.White
+        End If
+
+        If (B And (Not C) And (Not D)) Then
+            Button19.BackColor = Color.Red
+        Else
+            Button19.BackColor = Color.White
+        End If
+
+        If A Or ((Not B) And (Not C) And (Not D)) Or ((Not B) And C And D) Or (B And (Not C) And D) Or (B And C And (Not D)) Then
+            Button20.BackColor = Color.Red
+        Else
+            Button20.BackColor = Color.White
+        End If
+
+        If (Not B) Or ((Not C) And D) Or (C And (Not D)) Then
+            Button21.BackColor = Color.Red
+            Button22.BackColor = Color.Red
+            Button25.BackColor = Color.Red
+        Else
+            Button21.BackColor = Color.White
+            Button22.BackColor = Color.White
+            Button25.BackColor = Color.White
+        End If
+
+        If (Not B) Or (Not C) Or (Not D) Then
+            Button24.BackColor = Color.Red
+        Else
+            Button24.BackColor = Color.White
+        End If
+
+        If second = 9 Then
+            second = -1
+        End If
+    End Sub
+End Class
